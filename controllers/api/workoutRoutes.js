5ebcde883993c6f59e876d4a48241f93a9d62256
pconsag <pconsagra@g.ucla.edu>
2021-08-16 20:22:34 -0700

    add excercise routes

--- controllers/api/workoutRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const path = require("path");
-
-// Show the exercise page
-router.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
-
-module.exports = router;
